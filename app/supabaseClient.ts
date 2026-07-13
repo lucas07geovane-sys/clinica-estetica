a54da145738d6ecb14ea6801e1340c3625b8903d
lucas07geovane-sys <lucas07geovane-sys@users.noreply.github.com>
2026-07-13 19:25:33 -0400

    atualizando chave de acesso correta

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://kxdatmpilygchplcuxqx.supabase.co';
-const supabaseKey = 'sb_publishable_UijhQBIt90PenY6nRUi9zg_zDxFEJgb';
-
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4ZGF0bXBpbHlnY2hwbGN1eHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NDczMzQsImV4cCI6MjA5OTUyMzMzNH0.J3w4tH8BUqfdL0ZBn0fQ68rzS9_pxcceLGomBdFlXzs';
 export const supabase = createClient(supabaseUrl, supabaseKey);
