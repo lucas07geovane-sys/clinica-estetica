d09415e675c4358683164ccaad4193337395732a
lucas07geovane-sys <lucas07geovane@gmail.com>
2026-07-13 22:42:42 -0400

    V2

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kxdatmpilygchplcuxqx.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_UijhQBIt90PenY6nRUi9zg_zDxFEJgb';
+const supabaseUrl ='https://kxdatmpilygchplcuxqx.supabase.co';
+const supabaseKey ='sb_publishable_UijhQBIt90PenY6nRUi9zg_zDxFEJgb';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
